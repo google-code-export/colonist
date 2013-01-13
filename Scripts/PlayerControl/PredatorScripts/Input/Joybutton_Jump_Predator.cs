e20c9e56155fe8d63230288b31323e2b969ca0cd
yinyuanqings@gmail.com <yinyuanqings@gmail.com@e06f39b5-6f07-5aa3-db8c-9286502e1cc2>
2013-01-13 08:12:19 +0000

    add Jump button

---
+++ Scripts/PlayerControl/PredatorScripts/Input/Joybutton_Jump_Predator.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Predator3rdPersonalJumpController))]
+[RequireComponent(typeof(PredatorPlayerStatus))]
+public class Joybutton_Jump_Predator : JoyButton {
+
+    private Predator3rdPersonalJumpController JumpController;
+    public GameGUIHelper.RectPosition Location = GameGUIHelper.RectPosition.BottomRight;
+    public Color BaseColor;
+    private float LastJumpTime = 0;
+
+    void Awake()
+    {
+        this.JoyButtonName = "Jump";
+        JumpController = this.GetComponent<Predator3rdPersonalJumpController>();
+        ValueOffsetModifier = 5;
+    }
+
+	// Use this for initialization
+	void Start () {
+        JoyButtonBound = GameGUIHelper.GetSquareOnGUICoordinate(Location, JoyButtonSize);
+        JoyButtonBound.x += JoyButtonScreenOffset.x;
+        JoyButtonBound.y += JoyButtonScreenOffset.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    /// <summary>
+    /// Call when touch.phase = Began
+    /// </summary>
+    /// <param name="touch"></param>
+    public override void onTouchBegin(Touch touch)
+    {
+        if (PredatorPlayerStatus.IsJumping == false)
+        {
+            base.onTouchBegin(touch);
+        }
+    }
+    /// <summary>
+    /// Call when touch.phase = Move
+    /// </summary>
+    /// <param name="touch"></param>
+    public override void onTouchMove(Touch touch)
+    {
+    }
+    /// <summary>
+    /// Call when touch.phase = Move
+    /// </summary>
+    /// <param name="touch"></param>
+    public override void onTouchStationary(Touch touch)
+    {
+    }
+
+    /// <summary>
+    /// Call when touch.phase = End
+    /// </summary>
+    /// <param name="touch"></param>
+    public override void onTouchEnd(Touch touch)
+    {
+        base.onTouchEnd(touch);
+        StartCoroutine(JumpController.Jump());
+    }
+
+    void OnGUI()
+    {
+        GUI.color = this.BaseColor;
+        Rect r = new Rect(JoyButtonBound.x + JoyButtonBoundOffset.x,
+            JoyButtonBound.y + JoyButtonBoundOffset.y,
+            JoyButtonSize, JoyButtonSize);
+        GUI.DrawTexture(r, ButtonTexture);
+    }
+}
